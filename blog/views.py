09d104b10013c91c44601b9765ca8cf953cfb436
astep2004 <astep2004@gmail.com>
2023-10-26 11:33:03 +0000

    add similar posts in post using Count

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
-
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -73,12 +73,20 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
+    
+    #Список похожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    print(post_tags_ids)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form, 
+                   'similar_posts': similar_posts,
+                   })
 
 
 def post_share(request, post_id):
